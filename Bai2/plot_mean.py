eaa1d55bfa97a8b03fb8c6b53fbd6a8f058c2dec
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-11-20 01:21:43 +0700

    adjust plot mean

--- Bai2/plot_mean.py
+++ Bai2/plot_mean.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+from const import *
 
 mean_file = os.path.join(os.path.dirname(__file__), 'mean.csv')
 
@@ -18,16 +20,24 @@ with open(mean_file, 'r') as csv_file:
         for i, value in enumerate(row):
             data[header[i]].append(complex(value))
 
-plt.subplot(5, 1, 1)
-# Plot the data
-plt.plot(data['a'], label='a')
-plt.subplot(5, 1, 2)
-plt.plot(data['e'], label='e')
-plt.subplot(5, 1, 3)
-plt.plot(data['i'], label='i')
-plt.subplot(5, 1, 4)
-plt.plot(data['o'], label='o')
-plt.subplot(5, 1, 5)
-plt.plot(data['u'], label='u')
+k = np.arange(1, N_FFT, 1)
+frequencies = k * SAMPLE_RATE / N_FFT
+w = frequencies[:int(N_FFT/2)]
 
+plt.subplot(5, 1, 1)
+plt.plot(w, data['a'])
+plt.ylabel('a')
+plt.subplot(5, 1, 2)
+plt.plot(w, data['e'])
+plt.ylabel('e')
+plt.subplot(5, 1, 3)
+plt.plot(w, data['i'])
+plt.ylabel('i')
+plt.subplot(5, 1, 4)
+plt.plot(w, data['o'])
+plt.ylabel('o')
+plt.subplot(5, 1, 5)
+plt.plot(w, data['u'])
+plt.ylabel('u')
+plt.xlabel('Frequency Axis (Hz)')
 plt.show()
