15ffd7a865e9c2a65ecd22ee29c0a2e74f8019c5
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-11-21 12:36:31 +0700

    add traintest file B3

---
+++ Bai3/main.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import torchaudio
+import torch
+import csv
+from kmeans_pytorch import kmeans
+from const import *
+from tqdm import tqdm
+from collections import OrderedDict
+
+
+def to_mfcc(waveform):
+    transform = torchaudio.transforms.MFCC(
+        sample_rate=SAMPLE_RATE,
+        n_mfcc=N_MFCC,
+        melkwargs={"n_fft": WIN_SIZE, "win_length": WIN_SIZE, "hop_length": HOP_SIZE,
+                   "n_mels": 23, "center": False},
+    )
+    mfccs = transform(waveform)
+    mfcc = torch.mean(mfccs, dim=2)
+    return mfcc[0]
+
+
+def concentrate_mfcc(people, vowel_path):
+    mfccs = []
+    for person in tqdm(people):
+        waveform, sample_rate = torchaudio.load(os.path.join(
+            os.path.dirname(__file__), 'train_clean', person, vowel_path))
+        # startIndex = less_variant_segment(waveform)
+        mfccs.append(to_mfcc(waveform[:, START_INDEX:END_INDEX]))
+    return torch.stack(mfccs)
+
+
+people = os.listdir(os.path.join(os.path.dirname(__file__), 'train_clean'))
+
+mfcc_clusters = {
+    'a': concentrate_mfcc(people, 'a.wav'),
+    'e': concentrate_mfcc(people, 'e.wav'),
+    'i': concentrate_mfcc(people, 'i.wav'),
+    'o': concentrate_mfcc(people, 'o.wav'),
+    'u': concentrate_mfcc(people, 'u.wav'),
+}
+
+data = {
+    'a': kmeans(num_clusters=NUM_CLUSTER, X=mfcc_clusters['a'])[1],
+    'e': kmeans(num_clusters=NUM_CLUSTER, X=mfcc_clusters['e'])[1],
+    'i': kmeans(num_clusters=NUM_CLUSTER, X=mfcc_clusters['i'])[1],
+    'o': kmeans(num_clusters=NUM_CLUSTER, X=mfcc_clusters['o'])[1],
+    'u': kmeans(num_clusters=NUM_CLUSTER, X=mfcc_clusters['u'])[1],
+}
+
+num_clusters = 0
+
+
+def euclidean(v1, v2):
+    return sum((p-q)**2 for p, q in zip(v1, v2)) ** .5
+
+
+def predict_one(data, person, vowel_path):
+    dist = {label: 99999 for label in data.keys()}
+    waveform, sample_rate = torchaudio.load(os.path.join(
+        os.path.dirname(__file__), 'test_clean', person, vowel_path))
+    mfcc = to_mfcc(waveform[:, START_INDEX:END_INDEX])
+    for label in data.keys():
+        for vector in data[label]:
+            dist[label] = min(dist[label], euclidean(vector, mfcc))
+    return min(dist, key=lambda k: dist[k])
+
+
+people = os.listdir(os.path.join(os.path.dirname(__file__), 'test_clean'))
+
+mean_file = os.path.join(os.path.dirname(__file__), 'kmean.csv')
+
+counter = {
+    'a': 0,
+    'e': 0,
+    'i': 0,
+    'o': 0,
+    'u': 0,
+}
+result = {
+    'a': counter.copy(),
+    'e': counter.copy(),
+    'i': counter.copy(),
+    'o': counter.copy(),
+    'u': counter.copy(),
+}
+for person in people:
+    result['a'][predict_one(data, person, 'a.wav')] += 1
+    result['e'][predict_one(data, person, 'e.wav')] += 1
+    result['i'][predict_one(data, person, 'i.wav')] += 1
+    result['o'][predict_one(data, person, 'o.wav')] += 1
+    result['u'][predict_one(data, person, 'u.wav')] += 1
+
+output = os.path.join(os.path.dirname(__file__), 'test.csv')
+# Write result to CSV
+with open(output, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    truths = result.keys()
+    predicts = counter.keys()
+    # Write header row
+    csv_writer.writerow(['']+list(predicts))
+
+    # Write result rows
+    for truth in truths:
+        row = [truth] + list(result[truth].values())
+        csv_writer.writerow(row)
